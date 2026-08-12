20d351f834f36a4b3aef98dba9a0a354fb604ca9
Adrian Rivarola <ad.rivarola18@gmail.com>
2026-08-12 04:28:07 -0500

    url

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io('http://localhost:5000', {
+export const socket = io('https://tree-work-backend.onrender.com', {
     forceNew: true,
     reconnection: true,
     reconnectionAttempts: 5,
